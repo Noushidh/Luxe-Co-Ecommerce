d9d84a234af78d56ff32d3b76f21a6a62ba16342
noushidh <noushidhk@gmail.com>
2025-12-15 16:56:10 +0530

    create utils files asyHandler and constants and paginate

---
+++ utils/paginate.js
@@ -0,0 +1,15 @@
+export const paginate = async (Model, page = 1, limit = 10, query = {}) => {
+    const skip = (page - 1) * limit;
+
+    const results = await Model.find(query)
+        .skip(skip)
+        .limit(limit)
+        .sort({ createdAt: -1 });
+
+    const total = await Model.countDocuments(query);
+    const totalPages = Math.ceil(total / limit);
+
+    return {
+        results,total,totalPages,currentPage: page
+          };
+};
