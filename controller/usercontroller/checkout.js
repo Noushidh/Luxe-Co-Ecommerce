0ef4040c1c3e4dc1c61c432e2cca711e114c0cfe
noushidh <noushidhk@gmail.com>
2025-12-31 19:52:21 +0530

    implement cap in admin side for coupen

--- controller/usercontroller/checkout.js
+++ controller/usercontroller/checkout.js
@@ -86,29 +86,50 @@ export const checkStockBeforeCheckout = asyncHandler(async (req, res) => {
 
 export const applyCoupen = asyncHandler(async (req, res) => {
     const { code } = req.body;
-    const userId = req.session.user;
-    console.log("code===", code);
-    const cart = await CartModel.findOne({ user: userId }).populate('items.productId')
-    const subTotal = cart.items.reduce((acc, item) => acc + (item.price * item.quantity), 0);
-    console.log(subTotal)
-    const coupon = await couponModel.findOne({ code: code, isActive: true })
+    const userId = req.session.user._id; 
 
-     if (coupon.usersUsed.includes(userId)) {
+    const cart = await CartModel.findOne({ user: userId }).populate('items.productId');
+    const subTotal = cart.items.reduce((acc, item) => acc + (item.price * item.quantity), 0);
+    
+    const coupon = await couponModel.findOne({ code: code, isActive: true });
+
+    if (!coupon) {
+        return res.status(404).json({ success: false, message: "Invalid or expired coupon code" });
+    }
+
+    if (coupon.usersUsed.includes(userId)) {
         return res.status(400).json({ success: false, message: "You have already used this coupon!" });
     }
 
-    let finalDiscountValue = coupon.discountType === 'percentage'
-        ? (subTotal * coupon.discountValue) / 100 : coupon.discountValue;
-
-    if (coupon.discountType === 'percentage' && coupon.maxDiscount && finalDiscountValue > coupon.maxDiscount) {
-        finalDiscountValue = coupon.maxDiscount;
+    if (subTotal < coupon.minPurchase) {
+        return res.status(400).json({ success: false, message: `Minimum purchase of ₹${coupon.minPurchase} required` });
     }
+
+    let finalDiscountValue = 0;
+
+    if (coupon.discountType === 'percentage') {
+        finalDiscountValue = (subTotal * coupon.discountValue) / 100;
+        
+        if (coupon.maxDiscountAmount && finalDiscountValue > coupon.maxDiscountAmount) {
+            finalDiscountValue = coupon.maxDiscountAmount;
+        }
+    } else {
+        finalDiscountValue = coupon.discountValue;
+    }
+
+    if (finalDiscountValue > subTotal) {
+        finalDiscountValue = subTotal;
+    }
+
     req.session.appliedCoupon = {
-        _id:coupon._id,
+        _id: coupon._id,
         code: coupon.code,
-        discountValue: finalDiscountValue
+        discountValue: Math.round(finalDiscountValue)
     };
 
-    console.log("finalDiscountValue", finalDiscountValue)
-    res.status(200).json({ success: true, message: "Coupon applied!", discount: finalDiscountValue });
-})
+    res.status(200).json({ 
+        success: true, 
+        message: "Coupon applied!", 
+        discount: req.session.appliedCoupon.discountValue 
+    });
+});
