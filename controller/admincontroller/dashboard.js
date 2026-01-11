82c2fe5d052532fc2443ea56833211ded8227451
noushidh <noushidhk@gmail.com>
2026-01-11 19:31:36 +0530

    refactor(dashboard): implement multi-stage aggregation for subcategory analytics
    - Fixed  accumulator errors for totalSold calculations.
    - Corrected field pathing for subCategory_id in Product-Order join.
    - Ensured all aggregation pipelines are wrapped in arrays to prevent Mongoose errors.

--- controller/admincontroller/dashboard.js
+++ controller/admincontroller/dashboard.js
@@ -1,8 +1,54 @@
-import admin from "../../models/adminmodel.js";
+import userModel from "../../models/usermodel.js";
+import orderModel from "../../models/ordermodel.js";
+import asyncHandler from "../../utils/asynHandler.js";
 
-export const load_dashboard = (req, res) => {
+export const load_dashboard = asyncHandler(async (req, res) => {
+  const totalUsers = await userModel.countDocuments({ isBlocked: false });
+  const totalOrders = await orderModel.countDocuments();
+
+  const topProducts = await orderModel.aggregate([
+    { $match: { status: "Delivered" } },
+    { $unwind: "$items" },
+    { $group: { _id: "$items.productId", name: { $first: "$items.productName" }, totalSolds: { $sum: "$items.quantity" }, imageUrl: { $first: "$items.image" } } }, { $sort: { totalSolds: -1 } }, { $limit: 10 }
+  ])
+
+  const topCategories = await orderModel.aggregate([
+    { $match: { status: "Delivered" } },
+    { $unwind: "$items" },
+    { $lookup: { from: "products", localField: "items.productId", foreignField: "_id", as: "productInfo" } },
+    { $unwind: "$productInfo" },
+    { $lookup: { from: "subcategories", localField: "productInfo.subCategory_id", foreignField: "_id", as: "subCategoryData" } },
+    { $unwind: "$subCategoryData" },
+    { $group: { _id: "$subCategoryData.category", count: { $sum: "$items.quantity" } } },
+    { $sort: { count: -1 } }
+  ]);
+
+  const topSubcategories = await orderModel.aggregate([
+    { $match: { status: "Delivered" } },
+    { $unwind: "$items" },
+    { $lookup: { from: "products", localField: "items.productId", foreignField: "_id", as: "productInfo" } },
+    { $unwind: "$productInfo" },
+    { $lookup: { from: "subcategories", localField: "productInfo.subCategory_id", foreignField: "_id", as: "subCategoryDetails" } },
+    { $unwind: "$subCategoryDetails" },
+    { $group: { _id: "$subCategoryDetails.subcategory", totalSold: { $sum: "$items.quantity" } } },
+    { $sort: { totalSold: -1 } }, { $limit: 10 }
+  ]);
+
+  const monthlyRevenueData = await orderModel.aggregate([
+    { $match: { status: "Delivered" } },
+    { $group: { _id: { $month: "$createdAt" }, total: { $sum: "$total" } } },
+    { $sort: { "_id": 1 } }
+  ]);
+
   res.render("admin/layout", {
     title: "Dashboard",
     body: "./dashboard",
+    totalUsers,
+    totalOrders,
+    totalRevenue: 5,
+    topProducts,
+    topCategories,
+    topSubcategories,
+    monthlyRevenue: JSON.stringify(monthlyRevenueData)
   });
-};
+}) 
